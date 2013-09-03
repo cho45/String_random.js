f7a40546915d30f8f05a1866a5f4aa3bed79af72
cho45 <cho45@lowreal.net>
2013-09-03 21:01:30 +0900

    Implement reference.

--- test/test.js
+++ test/test.js
@@ -17,6 +17,7 @@ var patterns = [
 	/^\W{4}$/,
 	/^\w{1,}$/,
 	/^\w{1,2}$/,
+	/^[\d]$/,
 	/^[abc]+$/,
 	/^[$-]+$/, // no warnings
 	/^[a-c]+$/, // no warnings
@@ -25,10 +26,13 @@ var patterns = [
 	/^ab*$/,
 	/^...$/,
 	/^aa|bb$/,
+	/^(a|b)$/,
 	/^(aa|bb)$/,
 	/^(aa|bb(cc|dd))$/,
 	/^(a(xx|yy)a|bb(cc|dd))$/,
-	/http:\/\/[a-z]{3,8}\.example\.com\/([a-z\d]+\/){3}/
+	/http:\/\/[a-z]{3,8}\.example\.com\/([a-z\d]+\/){3}/,
+	/(?:<(p|div)>foo<\/\1>)+/,
+	/(aa|(bb|cc))\1\2/
 ];
 
 for (var x = 0, pattern; (pattern = patterns[x]); x++) {
